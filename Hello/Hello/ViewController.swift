6adc00894f657190c139ffb301cda91968d06247
gohta <okuno@freemake.co.jp>
2020-04-02 16:05:01 +0900

    配置した要素をVirwController.swiftと接続。 buttonを押下するとLabelの文字が変わるようにした。

--- Hello/Hello/ViewController.swift
+++ Hello/Hello/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBAction func sayHello() {
+        label.text = "こんにちは"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
